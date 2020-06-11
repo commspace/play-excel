a4c58dc40c659db6472375d9efbcff6ed139c48d
Johan Vosloo <johan@headspacetech.com>
2020-06-11 20:30:14 +0200

    Small refactoring

--- src/play/modules/excel/Plugin.java
+++ src/play/modules/excel/Plugin.java
@@ -44,7 +44,7 @@ import play.vfs.VirtualFile;
 
 public class Plugin extends PlayPlugin {
     
-    public static final String VERSION = "1.5.3";
+    public static final String VERSION = "2.0.1";
 
     public static PlayPlugin templateLoader = null;
     
